259052e8dc0ce4d925847049be68ee81aea403b5
ONZ1 <onzi1230@gmail.com>
2021-05-10 23:34:35 +0900

    Create main.cpp

---
+++ pointer001/pointer001/main.cpp
@@ -0,0 +1,14 @@
+#include "change.h"
+
+void main() {
+	int number[5] = { 10,20,30,40,50 };
+	int* num = number;
+
+	printf("기존 배열:");
+	out(number);
+
+	SwapCallByReference(num, input());
+	
+	printf("변경 후 배열:");
+	out(number);
+}
